fa723bc71ef02e46d09869ad61c657806ef5ecac
Andy Qi <ydnaiq@outlook.com>
2025-05-18 10:57:25 -0400

    fixed bug not detecting collision

--- Documents/masseyhacks2025/EnemyTrashCan/enemy_trashcan.tres.gd
+++ Documents/masseyhacks2025/EnemyTrashCan/enemy_trashcan.tres.gd
@@ -2,6 +2,5 @@ extends Area2D
 
 
 func _on_body_entered(_body):
-	print("entered")
 	if Global.size > -1:
 		Global.size -= 1
--- Documents/masseyhacks2025/scripts/rich_text_label.gd
+++ Documents/masseyhacks2025/scripts/rich_text_label.gd
@@ -10,16 +10,12 @@ func _process(_delta):
 		if courotine_started == false:
 			courotine_started = true
 			if Global.scene == 1:
-				print("started")
 				_type("I love the city! I wish the trash would go away tho")
-			if Global.scene == 2:
+			else:
 				_type("the city is crumbling... under the weight of the trash...")
-			if Global.scene == 3:
-				_type("...")
 
 
 func _type(string):
-	print("passed")
 	text = ""
 	for i in range(len(string)):
 		a = string[i]
